80db926dae3f41f4f8b6ec9e83a7128b44467a41
Richard Clark <richard@richard-clarks-macbook.local>
2010-10-16 23:10:37 +1300

    First hit at templates

--- client/world.js
+++ client/world.js
@@ -32,6 +32,7 @@ world.builder = function (app, parent, config) {
         if (!element.type == 'Template') {
             continue;
         }
+        console.log("Found template");
         
         result[k] = new world[element.type](app, k, parent, element);
     }
