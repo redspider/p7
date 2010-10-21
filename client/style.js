469dd57a648e148bdff9b903ab581c73ca60555e
Richard Clark <richard@richard-clarks-macbook.local>
2010-10-22 01:58:48 +1300

    More colours

--- client/style.js
+++ client/style.js
@@ -1,7 +1,15 @@
 com.p7.style = {
     color: {
-        approved: "rgba(212,212,121,X)",
-        let_me_in: "rgba(189,212,121,X)",
-        myrtle_sand: "rgba(212,189,121,X)"
+        yellow: "rgba(212,212,121,X)",
+        light_green: "rgba(189,212,121,X)",
+        orange: "rgba(212,189,121,X)",
+        dark_orange: "rgba(212,171,121,X)",
+        rose: "rgba(212,148,121,X)",
+        light_purple: "rgba(198,113,163,X)",
+        purple: "rgba(166,111,194,X)",
+        dark_purple: "rgba(130,116,203,X)",
+        blue: "rgba(116,152,203,X)",
+        light_blue: "rgba(111,181,203,X)",
+        green: "rgba(116,203,116,X)"
     }
 };
