b03c95892c589aaf7c3dac92b1b9bfe4015825dd
Richard Clark <richard@richard-clarks-macbook.local>
2010-10-22 02:20:34 +1300

    More colours

--- client/style.js
+++ client/style.js
@@ -1,8 +1,8 @@
 com.p7.style = {
     color: {
         mem_used: "rgba(212,189,121,X)",
-        mem_buffer: "rgba(200,170,82,X)",
-        mem_cached: "rgba(175,145,56,X)",
+        mem_buffer: "rgba(188,164,94,X)",
+        mem_cached: "rgba(152,133,79,X)",
         yellow: "rgba(212,212,121,X)",
         light_green: "rgba(189,212,121,X)",
         orange: "rgba(212,189,121,X)",
